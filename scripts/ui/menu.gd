a19a1cb4892e4cabf0774dee5e99325289790590
Isak <shoeboss69@gmail.com>
2025-03-16 14:54:24 +0100

    add main menu

---
+++ scripts/ui/menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func _ready() -> void:
+	$VBoxContainer/StartButton.grab_focus()
+
+
+func _on_start_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_level_select_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
