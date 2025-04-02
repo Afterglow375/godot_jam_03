d1c96eaa107618fb886513650cb90df401e15f45
Alex Tatusko <afterglow376@gmail.com>
2025-04-02 11:27:44 -0400

    Added scene transitions

--- scripts/systems/scene_manager.gd
+++ scripts/systems/scene_manager.gd
@@ -8,12 +8,18 @@ signal scene_change_started(new_scene_path)
 ## Emitted when a scene change operation has completed
 signal scene_change_completed(new_scene_path)
 
+@onready var animation_player = $AnimationPlayer
+
 ## Changes to a new scene using the provided path
 ## @param new_scene_path: String - Path to the scene file to load
 ## @return Error code from the scene change operation
 func change_scene(new_scene_path: String) -> Error:
+	animation_player.play("dissolve", 0.0, 2.0)
+	await animation_player.animation_finished
 	scene_change_started.emit(new_scene_path)
 	var err = get_tree().change_scene_to_file(new_scene_path)
+	var anim_length = animation_player.get_animation("dissolve").length
+	animation_player.play("dissolve", 0.0, -2.0, anim_length)
 	if err == OK:
 		# Defer the completion signal to ensure it fires after the scene is loaded
 		call_deferred("_emit_scene_change_completed", new_scene_path)
@@ -29,7 +35,7 @@ func _emit_scene_change_completed(scene_path: String) -> void:
 func reload_scene() -> Error:
 	var current_scene = get_tree().current_scene
 	if current_scene and current_scene.scene_file_path:
-		return change_scene(current_scene.scene_file_path)
+		return await change_scene(current_scene.scene_file_path)
 	else:
 		push_error("Could not reload scene: current scene path is null")
 		return ERR_CANT_CREATE 
